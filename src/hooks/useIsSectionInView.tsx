ed34ec7cf023528af6ac8c9071b64a946dc1e169
LongDDH <48487369+DDHLong@users.noreply.github.com>
2025-02-18 22:44:20 +0700

    Update useIsSectionInView.tsx

--- src/hooks/useIsSectionInView.tsx
+++ src/hooks/useIsSectionInView.tsx
@@ -4,7 +4,7 @@ import { useRef } from "react";
 const useIsSectionInView = () => {
   const projectRef = useRef(null);
   const isProjectInView = useInView(projectRef, {
-    amount: 0.5,
+    amount: 0.6,
   });
   const aboutRef = useRef(null);
   const isAboutInView = useInView(aboutRef, {
